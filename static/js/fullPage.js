963a6b738a89bfdba54a2013e86c24d9ce6f7074
us1aaron <aaron.li@stonybrook.edu>
2023-07-06 03:47:43 -0400

    update backrgound music and mute option

--- static/js/fullPage.js
+++ static/js/fullPage.js
@@ -1,5 +1,14 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const audio = document.querySelector("#audio");
+const audioToggle = document.querySelector("#audioToggle");
+audioToggle.addEventListener("change", () => {
+  if (audioToggle.checked) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+});
 gsap.to(".aboutTwoLineMask", {
   stagger: 1,
   scrollTrigger: {
@@ -13,41 +22,41 @@ gsap.to(".aboutTwoLineMask", {
 });
 
 gsap.to(".aboutThreeLineOne", {
-    scrollTrigger: {
-        trigger: '.aboutThreeLineOne',
-        start: "top 100%",
-        end: 'bottom 10%',
-        scrub: 4,
-    },
-    transform: 'translateX(2%)',
-})
+  scrollTrigger: {
+    trigger: ".aboutThreeLineOne",
+    start: "top 100%",
+    end: "bottom 10%",
+    scrub: 4,
+  },
+  transform: "translateX(2%)",
+});
 
 gsap.from(".aboutThreeLineTwo", {
-    scrollTrigger: {
-        trigger: '.aboutThreeLineTwo',
-        start: "top 100%",
-        end: 'bottom 10%',
-        scrub: 4,
-    },
-    transform: 'translateX(8%)',
-})
+  scrollTrigger: {
+    trigger: ".aboutThreeLineTwo",
+    start: "top 100%",
+    end: "bottom 10%",
+    scrub: 4,
+  },
+  transform: "translateX(8%)",
+});
 
 gsap.to(".aboutThreeLineThree", {
-    scrollTrigger: {
-        trigger: '.aboutThreeLineThree',
-        start: "top 100%",
-        end: 'bottom 10%',
-        scrub: 4,
-    },
-    transform: 'translateX(0%)',
-})
+  scrollTrigger: {
+    trigger: ".aboutThreeLineThree",
+    start: "top 100%",
+    end: "bottom 10%",
+    scrub: 4,
+  },
+  transform: "translateX(0%)",
+});
 
 gsap.from(".aboutThreeLineFour", {
-    scrollTrigger: {
-        trigger: '.aboutThreeLineFour',
-        start: "top 100%",
-        end: 'bottom 10%',
-        scrub: 4,
-    },
-    transform: 'translateX(8%)',
-})
+  scrollTrigger: {
+    trigger: ".aboutThreeLineFour",
+    start: "top 100%",
+    end: "bottom 10%",
+    scrub: 4,
+  },
+  transform: "translateX(8%)",
+});
