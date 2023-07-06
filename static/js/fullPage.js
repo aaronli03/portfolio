f258c1bf9c76b03afd8758a182306703b20e03fb
us1aaron <aaron.li@stonybrook.edu>
2023-07-05 22:21:57 -0400

    minor changes to about me section and completed passions section

--- static/js/fullPage.js
+++ static/js/fullPage.js
@@ -5,10 +5,39 @@ gsap.to(".aboutTwoLineMask", {
   scrollTrigger: {
     trigger: ".aboutTwoLineMask",
     start: "top 80%",
-    end: "bottom 40%",
-    scrub: true,
-    markers: true,
+    end: "bottom 20%",
+    scrub: 0.5,
   },
   width: "100%",
-  duration: 1.5,
+  duration: 1.3,
 });
+
+gsap.to(".aboutThreeLineOne", {
+    scrollTrigger: {
+        trigger: '.aboutThreeLineOne',
+        start: "top 80%",
+        end: 'bottom 20%',
+        scrub: 1.5,
+    },
+    transform: 'translateX(0%)',
+})
+
+gsap.from(".aboutThreeLineTwo", {
+    scrollTrigger: {
+        trigger: '.aboutThreeLineTwo',
+        start: "top 100%",
+        end: 'bottom 20%',
+        scrub: 1.5,
+    },
+    transform: 'translateX(20%)',
+})
+
+gsap.to(".aboutThreeLineThree", {
+    scrollTrigger: {
+        trigger: '.aboutThreeLineThree',
+        start: "top 100%",
+        end: 'bottom 20%',
+        scrub: 1.5,
+    },
+    transform: 'translateX(0%)',
+})
