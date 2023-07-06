485c646794ccea4d7cee43143d0d271074fe0db3
us1aaron <aaron.li@stonybrook.edu>
2023-07-05 22:59:43 -0400

    reformate passions section

--- static/js/fullPage.js
+++ static/js/fullPage.js
@@ -15,29 +15,39 @@ gsap.to(".aboutTwoLineMask", {
 gsap.to(".aboutThreeLineOne", {
     scrollTrigger: {
         trigger: '.aboutThreeLineOne',
-        start: "top 80%",
-        end: 'bottom 20%',
-        scrub: 1.5,
+        start: "top 100%",
+        end: 'bottom 10%',
+        scrub: 4,
     },
-    transform: 'translateX(0%)',
+    transform: 'translateX(2%)',
 })
 
 gsap.from(".aboutThreeLineTwo", {
     scrollTrigger: {
         trigger: '.aboutThreeLineTwo',
         start: "top 100%",
-        end: 'bottom 20%',
-        scrub: 1.5,
+        end: 'bottom 10%',
+        scrub: 4,
     },
-    transform: 'translateX(20%)',
+    transform: 'translateX(8%)',
 })
 
 gsap.to(".aboutThreeLineThree", {
     scrollTrigger: {
         trigger: '.aboutThreeLineThree',
         start: "top 100%",
-        end: 'bottom 20%',
-        scrub: 1.5,
+        end: 'bottom 10%',
+        scrub: 4,
     },
     transform: 'translateX(0%)',
+})
+
+gsap.from(".aboutThreeLineFour", {
+    scrollTrigger: {
+        trigger: '.aboutThreeLineFour',
+        start: "top 100%",
+        end: 'bottom 10%',
+        scrub: 4,
+    },
+    transform: 'translateX(8%)',
 })
